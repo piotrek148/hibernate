4de6a8b55407cd98958a5fad38bb27514528595d
Piotr Kukla <ptrkukla148@gmail.com>
2020-08-22 15:36:05 +0200

    z dependencjami

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * ... comment class...
+ *
+ * @author ptrkukla148@gmail.com
+ * @since 22.08.2020
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
